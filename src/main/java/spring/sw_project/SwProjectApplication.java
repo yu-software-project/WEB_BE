a2667cfd8cea65c71b33e2a93588593f0fafc34d
minju <minju9711@naver.com>
2024-10-15 03:10:11 +0900

    [chore] 깃 저장소 생성

---
+++ src/main/java/spring/sw_project/SwProjectApplication.java
@@ -0,0 +1,13 @@
+package spring.sw_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/sw_project/SwProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.sw_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
